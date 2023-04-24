8824655d3029a6ddeb2252c1c52e0547963233f8
jakub-ner <kubaner1@gmail.com>
2023-04-24 16:33:42 +0200

    login demo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 import { getSubjectListByLanguageAndTeacherName } from './utils/SubjectUtils';
-import App from './App';
+import GoogleSigninSampleApp from "./login";
 import { getTeacherListByLanguage } from './utils/TeacherUtils';
 import { LANGUAGE } from './utils/Language';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(GoogleSigninSampleApp);
